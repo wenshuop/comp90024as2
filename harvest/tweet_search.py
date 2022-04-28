f1f59f8347a4e17ec57e79f4448cd32f54680fe1
cocoqiu51 <81216697+cocoqiu51@users.noreply.github.com>
2022-04-29 02:19:48 +1000

    tweet harvest - search update
    tweet harvest - search update

--- harvest/tweet_search.py
+++ harvest/tweet_search.py
@@ -11,8 +11,9 @@ def load_cities(file_name):
         file = json.load(f)
         cities = defaultdict(lambda: dict())
         for city in file:
-            cities[city['name']]['id'] = city['id']
-            cities[city['name']]['full_name'] = city['full_name']
+            id = city['id']
+            cities[id]['name'] = city['name']
+            cities[id]['full_name'] = city['full_name']
             lng_lst = []
             lat_lst = []
             for (lng, lat) in city['bounding_box']['coordinates'][0]:
@@ -20,7 +21,7 @@ def load_cities(file_name):
                 lat_lst.append(lat)
             lng_lst.sort()
             lat_lst.sort()
-            cities[city['name']]['bounding_box'] = [lng_lst[0], lat_lst[0], lng_lst[-1], lat_lst[-1]]
+            cities[id]['bounding_box'] = [lng_lst[0], lat_lst[0], lng_lst[-1], lat_lst[-1]]
         return cities
 
 # get a database if exits, or create a new database if not exits
@@ -31,16 +32,18 @@ def find_or_create_db(server, db_name):
         return server.create(db_name)
 
 # save each tweet into the database
-def save_tweet(tweet, database):
+def save_tweet(database, tweet, city_id, city_details):
     if str(tweet.id) not in database:
         id, rev = database.save({"_id": str(tweet.id),
                                  "created_at": str(tweet.created_at),
                                  "text": tweet.text,
-                                 "city_name": city,
-                                 "city_id": city_info['id'],
-                                 "city_full_name": city_info["full_name"],
-                                 "city_coordinates": city_info["bounding_box"]})
+                                 "city_name": city_details['name'],
+                                 "city_id": city_id,
+                                 "city_full_name": city_details["full_name"],
+                                 "city_coordinates": city_details["bounding_box"]})
         print("tweet saved", id)
+    else:
+        print("tweet already exits")
 
 # load search start/end time from file "search_start_times.csv"
 def load_search_times(file_name, city):
@@ -53,7 +56,7 @@ def load_search_times(file_name, city):
         start_time = datetime.fromisoformat(start_time)
         end_time = datetime.fromisoformat(end_time)
     except:
-        end_time, start_time = datetime.now(), (datetime.now() - timedelta(days=30))
+        end_time, start_time = (datetime.now() - timedelta(days=1)), (datetime.now() - timedelta(days=30))
     return start_time, end_time
 
 # update next search time for a specific city to "search_start_times.csv"
@@ -76,10 +79,10 @@ if __name__ == '__main__':
     cities = load_cities("australia_cities.json")
     break_loop = False
     while True:
-        for city, city_info in cities.items():
-            query = "lang:en place:" + city_info['id']
+        for city_id, city_details in cities.items():
+            query = "lang:en place:" + city_id
             # load search time from "search_start_times.csv"
-            start_time, end_time = load_search_times("search_start_times.csv", city)
+            start_time, end_time = load_search_times("search_start_times.csv", city_details['name'])
             if end_time.year < 2021:
                 break_loop = True
                 continue
@@ -91,7 +94,7 @@ if __name__ == '__main__':
                 raise RuntimeError(resp.errors)
             if resp.data:
                 for tweet in resp.data:
-                    save_tweet(tweet, database)
+                    save_tweet(database, tweet, city_id, city_details)
 
             while resp.meta["next_token"]:
                 resp = client.search_all_tweets(query, tweet_fields=["created_at", "geo"], max_results=500,
@@ -101,12 +104,13 @@ if __name__ == '__main__':
                     raise RuntimeError(resp.errors)
                 if resp.data:
                     for tweet in resp.data:
-                        save_tweet(tweet, database)
+                        save_tweet(database, tweet, city_id, city_details)
+
             # update the search time for this city in "search_start_times.csv"
-            update_search_times("search_start_times.csv", city, start_time)
-            print(city, "tweets harvest finished from", start_time, "to", end_time)
+            update_search_times("search_start_times.csv", city_details['name'], start_time)
+            print(city_details['name'], "tweets harvest finished from", start_time, "to", end_time)
 
         if break_loop:
             break
 
-    print("Tweet Searching end.")
+    print("Tweet Search end.")
