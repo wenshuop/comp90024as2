05197bb8a6e81669cb382f26e5a87a639824feb1
VictorOfSun <51089144+VictorOfSun@users.noreply.github.com>
2022-05-01 00:31:29 +0800

    modify web

--- website_build/app.py
+++ website_build/app.py
@@ -1,18 +1,34 @@
 #encoding:utf-8
 import os
-from flask import Flask, jsonify, request, render_template
-# import simplejson as json
-app = Flask(__name__)
-# import models
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pl,kuhb10.@127.0.0.1/fund'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+from flask import Flask, jsonify, request, render_template
+import simplejson as json
+import flaskext.couchdb
+
+app = Flask(__name__)
+
+active_users_view = flaskext.couchdb.ViewDefinition('demo', 'active', '''\
+function (doc) {
+  emit(doc.name, doc.value);
+}''')
 
 @app.route('/')
-def index():
-    return render_template('route_one.html')
+def index():  # put application's code here
+    docs = []
+    for row in active_users_view():
+        docs.append({'name': row.key, 'value': row.value})
+    return render_template('route_one.html', docs=json.dumps(docs))
 
 
 if __name__ == '__main__':
-    # models.db.init_app(app)
-    app.run(host='0.0.0.0', port=5000)
+    app.config.update(
+        DEBUG=True,
+        COUCHDB_SERVER='http://admin:123456@localhost:5984/',
+        COUCHDB_DATABASE='demo'
+    )
+    # set up couchdb
+    manager = flaskext.couchdb.CouchDBManager()
+    manager.setup(app)
+    # Install the view
+    manager.add_viewdef(active_users_view)
+    app.run(host='0.0.0.0', port=8090)
